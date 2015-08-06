0b443605fda2296e566f6652de1926f1c36ca86e
Leroi Dejesa <leroi@dealerpeak.com>
2015-08-06 15:21:23 -0700

    hotfix: edit a comment/variable name or two.

--- vehicles/vehicles.go
+++ vehicles/vehicles.go
@@ -16,6 +16,7 @@ type Vehicle struct {
 	Stocknumber string `json:"stocknumber"`
 }
 
+// Vlist is the primary data cache
 var Vlist = []Vehicle{
 	Vehicle{
 		Year:        2003,
@@ -31,12 +32,12 @@ var Vlist = []Vehicle{
 	},
 }
 
-var VArray [][]string
+// Vslices holds pre-parsed slices of vehicle data
+var Vslices [][]string
 
-// Push is the places photo files into a multidimensional slice for further parsing
-func Push(p []string) {
-	VArray = append(VArray, p)
-	// VArray = append(VArray[:1], VArray[1+1:]...)
+// push places photo files into a multidimensional slice (Vslices) for func parseFiles()
+func push(p []string) {
+	Vslices = append(Vslices, p)
 }
 
 // ImportPhotoData is an exported function that
@@ -46,12 +47,12 @@ func ImportPhotoData(root string) {
 	dir := os.ExpandEnv(root)
 	numImported := 0
 	var walkpath = func(path string, f os.FileInfo, err error) error {
-		// Considering using ioutil.ReadDir() instead of filepath.Walk
+		// Considering using ioutil.ReadDir() instead of filepath.Walk. Tabled for now.
 		if f.IsDir() {
 			return nil
 		}
 		pathinfo := strings.Split(f.Name(), "_")
-		Push(pathinfo)
+		push(pathinfo)
 		numImported++
 		return nil
 	}
@@ -62,11 +63,11 @@ func ImportPhotoData(root string) {
 
 // organizes import results and adds vehicles to memory in proper format.
 func parseFiles() {
-	for _, file := range VArray {
+	for _, file := range Vslices {
 		year, _ := strconv.Atoi(file[0])
 		make := file[1]
 		model := file[2]
-		// in order to trim off the '-[view int].jpg' :
+		// in order to trim off the '-[imgview int].jpg' :
 		stockfile := file[3]
 		stockExt := filepath.Ext(stockfile)
 		stock := stockfile[0 : len(stockfile)-(len(stockExt)+2)]
